feb3f83d1e8038bd758d90a951fce3b1e954a92a
cmmille <cmmiller.bio@gmail.com>
2023-08-18 10:20:45 -0700

    Add named slot

--- ToDoItem.js
+++ ToDoItem.js
@@ -10,29 +10,37 @@ class ToDoItem extends HTMLElement {
   createTemplate() {
     const template = document.createElement("template");
     template.innerHTML = `
-          <style>
-              :host {
-                  display: block;
-                  margin: 10px;
-              }
-              label {
-                  display: flex;
-                  align-items: center;
-              }
-              input[type="checkbox"] {
-                  margin-right: 10px;
-              }
-              h1 {
-                  color: green;
-                  margin: 0;
-                  font-size: 1rem;
-              }
-          </style>
-          <label>
-              <input type="checkbox">
-              <h1><slot></slot></h1>
-          </label>
-      `;
+            <style>
+                :host {
+                    display: block;
+                    margin: 10px;
+                }
+                label {
+                    display: flex;
+                    align-items: center;
+                }
+                input[type="checkbox"] {
+                    margin-right: 10px;
+                }
+                h1 {
+                    color: green;
+                    margin: 0;
+                    font-size: 1rem;
+                }
+                .description {
+                    color: grey;
+                    font-size: 0.8rem;
+                    margin-left: 1rem;
+                }
+            </style>
+            <label>
+                <input type="checkbox">
+                <h1><slot></slot></h1>
+                <h2 class="description">
+                    <slot name="description"></slot>
+                </h2>
+            </label>
+        `;
     return template;
   }
 }

18b2e958b66cae6352543eaad98f1f4a81a5596c
cmmille <cmmiller.bio@gmail.com>
2023-08-18 17:15:33 -0700

    Add propped component

--- src/components.js
+++ src/components.js
@@ -1,3 +1,4 @@
 export { NameTag } from "./components/NameTag.js";
 export { CheckBox } from "./components/CheckBox.js";
 export { ToDoList } from "./components/ToDoList.js";
+export { SimpleGreeting } from "./components/SimpleGreeting.js";
---
+++ src/components/SimpleGreeting.js
@@ -0,0 +1,24 @@
+const { html, css, LitElement } = window;
+
+export class SimpleGreeting extends LitElement {
+  static styles = css`
+    p {
+      color: blue;
+    }
+  `;
+
+  static properties = {
+    name: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.name = "Somebody";
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+
+customElements.define("simple-greeting", SimpleGreeting);
